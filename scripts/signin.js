20e0d05f3a9965944c310e80e9e93abaae22b8f2
HyeyoonS <foryou9097@gmail.com>
2024-06-02 04:36:18 +0900

    [#M3] feat : signin.js 정의

---
+++ scripts/signin.js
@@ -0,0 +1,50 @@
+import { errorMessageStop } from "./errors/errors.js";
+import { checkPassword } from "./input/checkPassword.js";
+import { toggleImage } from "./toggleImage.js";
+import { inputEmail, inputPassword } from "./constants.js";
+import { checkEmailFormat } from "./input/checkEmailFormat.js";
+import { validAccount } from "./auth/validConfirm.js";
+console.log("실행되나?");
+
+const eyeImagePasswordEl = document.querySelector("#eyeImage-password");
+const eyeImagePassword = eyeImagePasswordEl.children[0];
+const submitButton = document.querySelector('button[type="submit"]');
+const loginForm = document.querySelector("form");
+
+console.log("실행되나?2222");
+//<눈모양 아이콘 적용, 비밀번호 입력타입 변경>
+eyeImagePassword.addEventListener("click", () => {
+  toggleImage(eyeImagePassword, inputPassword);
+});
+console.log("실행되나?3333");
+//<입력하는 동안에는 에러메시지 안 보이게 하기 >
+inputEmail.addEventListener("input", errorMessageStop);
+inputPassword.addEventListener("input", errorMessageStop);
+
+// <이메일 형식검증, 오류메시지 출력 >
+inputEmail.addEventListener("focusout", checkEmailFormat);
+
+//<비밀번호 빈 값일때 오류메시지 출력>
+inputPassword.addEventListener("focusout", checkPassword);
+
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const email = inputEmail.value.trim();
+  const password = inputPassword.value;
+  validAccount(email, password);
+});
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const email = inputEmail.value.trim();
+  const password = inputPassword.value;
+  validAccount(email, password);
+});
+
+//자동로그인
+window.addEventListener("load", () => {
+  const storedAccessToken = localStorage.getItem("accessToken");
+  if (storedAccessToken) {
+    window.location.href = "/folder";
+  }
+});
