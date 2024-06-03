b1e8e1114469f62bc5aa6aae87bb30b6fbdf3907
HyeyoonS <foryou9097@gmail.com>
2024-06-03 10:28:36 +0900

    [#M3] fix : alert 메세지가 아니라 alert가 나타나도록 수정

--- scripts/auth/validConfirm.js
+++ scripts/auth/validConfirm.js
@@ -7,24 +7,31 @@ import {
   passwordErrorMessage,
 } from "../constants.js";
 
+import { showModal } from "../components/modal.js";
+
 // <아이디&비밀번호 올바르게 입력했을 경우 /items로 이동하고 아닌 경우 확인메시지 출력>
 function validAccount(email, password) {
   const accountMatch = USER_DATA.find((account) => account.email === email);
 
-  if (accountMatch?.password === password) {
-    window.location.href = "./items.html";
-  }
+  //   if (accountMatch) {
+  //     if (accountMatch.password === password) {
+  //       window.location.href = "./items.html"; //
+  //     } else {
+  //       alert("존재하지 않는 이메일, 혹은 비밀번호입니다.");
+  //     }
+  //   } else {
+  //     alert("존재하지 않는 이메일, 혹은 비밀번호입니다.");
+  //   }
+  // }
 
-  if (accountMatch?.password !== password) {
-    addErrorStyle(
-      inputPassword,
-      passwordErrorMessage,
-      "비밀번호를 확인해주세요"
-    );
-  }
-
-  if (!accountMatch) {
-    addErrorStyle(inputEmail, emailErrorMessage, "이메일을 확인해주세요.");
+  if (accountMatch) {
+    if (accountMatch.password === password) {
+      window.location.href = "./items.html"; // Redirect to /items.html
+    } else {
+      showModal("존재하지 않는 이메일, 혹은 비밀번호입니다.");
+    }
+  } else {
+    showModal("존재하지 않는 이메일, 혹은 비밀번호입니다.");
   }
 }
 
