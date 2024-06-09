e91c023c7dd0ad590ffcb5a9cbf4769fc735583e
HyeyoonS <foryou9097@gmail.com>
2024-06-09 16:30:17 +0900

    [#M5] feat : App에서 CardList 불러오기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 // import "./App.css";
+import { useEffect, useState, useCallback } from "react";
 import Nav from "./components/Nav";
+import CardList from "components/CardList";
+import getProducts from "utils/api";
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  const fetchCards = useCallback(async () => {
+    try {
+      const productsList = await getProducts("/products");
+      setCards(productsList.list);
+      console.log(productsList.list);
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchCards();
+  }, [fetchCards]);
+
   return (
     <div>
-      <Nav />;
+      <Nav />
+      <CardList cards={cards} />
     </div>
   );
 }
