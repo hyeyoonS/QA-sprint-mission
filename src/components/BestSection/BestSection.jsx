757b1997cabbf1935ee57459a379a41bfa48a3fd
HyeyoonS <foryou9097@gmail.com>
2024-06-10 22:40:11 +0900

    [#M5] chore : cascading피하기 위한 className수정

--- src/components/BestSection/BestSection.jsx
+++ src/components/BestSection/BestSection.jsx
@@ -5,8 +5,8 @@ import BestCardList from "components/BestSection/BestCardList";
 const BestSection = ({ bestCards }) => {
   return (
     <section className="best_section_container">
-      <div className="title_bar">
-        <p className="title">베스트 상품</p>
+      <div className="best_title_bar">
+        <p className="best_title">베스트 상품</p>
       </div>
       <BestCardList bestCards={bestCards} sortOption={"좋아요순"} />
     </section>
