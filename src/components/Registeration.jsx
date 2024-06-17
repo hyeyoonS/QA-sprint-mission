2b00253b3ccaaac92ec1f56ebe7c243084e4d0ab
HyeyoonS <foryou9097@gmail.com>
2024-06-17 21:11:26 +0900

    [#M6] feat : getProducts 호출시 포맷 폼데이터→객체형태로 보내기, 이미지 업로드 기능,  tags옵션 수정

--- src/components/Registeration.jsx
+++ src/components/Registeration.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "./button/Button";
-import { postProducts } from "../utils/api";
+import { postProducts, postImage } from "../utils/api";
 import { ERROR_MESSAGES } from "../utils/error_constants";
 import useValidation from "../hooks/useValidation";
 import "./Registeration.css";
@@ -10,30 +10,61 @@ function Registration() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors: formErrors },
   } = useForm();
+
   const { errors: validationErrors, validate } = useValidation();
+  const [tags, setTags] = useState([]);
 
   const onSubmit = async (data) => {
     console.log("데이터확인:", data);
-    const formData = new FormData();
-    formData.append("image", data.image[0]);
-    formData.append("name", data.name);
-    formData.append("description", data.description);
-    formData.append("price", data.price);
-    formData.append("tags", data.tags);
+
+    const imageData = await imgToUrl(data.image[0]);
+
+    console.log(imageData);
+    const sendData = {
+      images: [imageData.url],
+      name: data.name,
+      description: data.description,
+      price: +data.price,
+      tags: tags,
+    };
 
     try {
-      const response = await postProducts(formData);
+      const response = await postProducts(sendData);
       console.log(response);
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
+  const imgToUrl = async (imgData) => {
+    const imageUrl = await postImage(imgData);
+    return imageUrl;
+  };
+
+  //유효성 검사 hook 사용을 위한 핸들러
   const handleBlur = (e) => {
     const { name, value } = e.target;
+    console.log(name, value);
     validate(name, value);
+  };
+
+  //엔터를 누르면 태그를 추가하고 띄어쓰기는 제거
+  const handleTagKeyPress = (e) => {
+    const inputTagName = e.target.value.trim();
+    console.log("이", inputTagName);
+    if (inputTagName.length > 5) return;
+
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const value = inputTagName;
+      if (value) {
+        setTags([...tags, value]);
+        setValue("tags", "");
+      }
+    }
   };
 
   return (
@@ -113,9 +144,15 @@ function Registration() {
           className="register_input"
           type="text"
           placeholder="태그를 입력해주세요"
-          {...register("tags", { required: true })}
+          onKeyDown={handleTagKeyPress}
+          {...register("tags")}
           onBlur={handleBlur}
         />
+        {tags.map((tag, index) => (
+          <div key={index} className="tag_item">
+            {tag}
+          </div>
+        ))}
         {(formErrors.tags || validationErrors.tags) && (
           <span className="error">
             {formErrors.tags ? formErrors.tags.message : validationErrors.tags}
