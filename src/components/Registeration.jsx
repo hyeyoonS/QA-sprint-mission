d6125f2f859b0afbe467df9e92480091426011c6
HyeyoonS <foryou9097@gmail.com>
2024-06-20 01:38:46 +0900

    [#M6] feat : 데이터 올바르게 유효성 검사한 후 유효한 경우에만 제출되도록 하기

--- src/components/Registeration.jsx
+++ src/components/Registeration.jsx
@@ -11,69 +11,97 @@ function Registration() {
   const {
     register,
     handleSubmit,
-    setValue,
     trigger,
-    formState: { errors: formErrors },
-  } = useForm();
+    clearErrors,
+    setError,
+    formState: { errors: formErrors, isValid },
+  } = useForm({ mode: "onChange" });
 
   const { errors: validationErrors, validate } = useValidation();
-  const [tags, setTags] = useState([]);
+  const [allTags, setAllTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
 
   const onSubmit = async (data) => {
-    console.log("데이터확인:", data);
+    // console.log("allTags:", allTags);
 
-    const imageData = await imgToUrl(data.image[0]);
+    // const imageData = await imgToUrl(data.image[0]);
 
-    console.log(imageData);
+    // console.log(imageData);
     const sendData = {
-      images: [imageData.url],
+      images: ["임시URL"],
+      // images: imageData.url || [imageData.url],
       name: data.name,
       description: data.description,
       price: +data.price,
-      tags: tags,
+      tags: allTags,
     };
 
-    try {
-      const response = await postProducts(sendData);
-      console.log(response);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+    // console.log(sendData);
+    //   try {
+    //     const response = await postProducts(sendData);
+    //     console.log(response);
+    //   } catch (error) {
+    //     console.error("Error:", error);
+    //   }
+    // };
 
-  const imgToUrl = async (imgData) => {
-    const imageUrl = await postImage(imgData);
-    return imageUrl;
+    // const imgToUrl = async (imgData) => {
+    //   const imageUrl = await postImage(imgData);
+    //   return imageUrl;
   };
 
   //유효성 검사 hook 사용을 위한 핸들러
-  const handleBlur = (e) => {
+  const handleBlur = async (e) => {
     const { name, value } = e.target;
     console.log(name, value);
-    validate(name, value);
+    if (register[name]) {
+      validate(name, value);
+      await trigger(name);
+    }
   };
 
-  //엔터를 누르면 태그를 추가하고 띄어쓰기는 제거
   const handleTagKeyPress = (e) => {
+    console.log("실행됐나?");
     const inputTagName = e.target.value.trim();
-    console.log("이", inputTagName);
-    if (inputTagName.length > 5) return;
-
     if (e.key === "Enter") {
       e.preventDefault();
-      const value = inputTagName;
-      if (value) {
-        setTags([...tags, value]);
-        setValue("tags", "");
+      if (!inputTagName) {
+        // 입력이 비어 있는 경우 오류 메시지 표시하고 종료
+        setError("tags", {
+          type: "manual",
+          message: "태그는 필수 입력 항목입니다",
+        });
+        return;
+      }
+      if (inputTagName.length > 5) {
+        // 5글자를 초과한 경우 오류 메시지 표시하고 종료
+        setError("tags", {
+          type: "manual",
+          message: "태그는 5글자 이내로 입력하세요",
+        });
+        // console.log("5글자넘으면안됨");
+        return;
+      }
+      // 오류가 없는 경우 태그 추가 및 입력 필드 초기화
+      if (inputTagName) {
+        setNewTag(""); // 먼저 newTag 상태를 비움
+        setAllTags([...allTags, inputTagName]);
+        // 오류 메시지 초기화
+        clearErrors("tags");
       }
     }
+  };
+
+  // 태그 삭제 함수
+  const handleTagDelete = (tagDelete) => {
+    setAllTags(allTags.filter((tag) => tag !== tagDelete));
   };
 
   return (
     <form className={styles.form_container} onSubmit={handleSubmit(onSubmit)}>
       <section className={styles.registration_title}>
         <h1>상품 등록하기</h1>
-        <Button color="blue" type="submit">
+        <Button color="blue" type="submit" disabled={!isValid}>
           등록
         </Button>
       </section>
@@ -87,9 +115,9 @@ function Registration() {
           {...register("image", { required: true })}
           onBlur={handleBlur}
         />
-        {formErrors.image && (
+        {/* {formErrors.image && (
           <span className={styles.error}>{ERROR_MESSAGES.image}</span>
-        )}
+        )} */}
       </fieldset>
 
       <fieldset>
@@ -100,7 +128,17 @@ function Registration() {
           }`}
           type="text"
           placeholder="상품명을 입력해주세요"
-          {...register("name", { required: true })}
+          {...register("name", {
+            required: "상품명은 필수 입력 항목입니다.",
+            minLength: {
+              value: 1,
+              message: "상품명은 최소 1자 이상이어야 합니다.",
+            },
+            maxLength: {
+              value: 10,
+              message: "상품명은 최대 10자 이내여야 합니다.",
+            },
+          })}
           onBlur={handleBlur}
         />
         {(formErrors.name || validationErrors.name) && (
@@ -115,7 +153,17 @@ function Registration() {
         <textarea
           className={`${formErrors.description ? styles.input_error : ""}`}
           placeholder="상품 소개를 입력해주세요"
-          {...register("description", { required: true })}
+          {...register("description", {
+            required: "상품 소개는 필수 입력 항목입니다.",
+            minLength: {
+              value: 10,
+              message: "상품 소개는 최소 10자 이상이어야 합니다.",
+            },
+            maxLength: {
+              value: 100,
+              message: "상품 소개는 최대 100자 이내여야 합니다.",
+            },
+          })}
           onBlur={handleBlur}
         ></textarea>
         {(formErrors.description || validationErrors.description) && (
@@ -135,7 +183,21 @@ function Registration() {
           }`}
           type="text"
           placeholder="판매 가격을 입력해주세요"
-          {...register("price", { required: true })}
+          {...register("price", {
+            required: "판매 가격은 필수 입력 항목입니다.",
+            minLength: {
+              value: 1,
+              message: "판매 가격은 최소 1자 이상이어야 합니다.",
+            },
+            maxLength: {
+              value: 100,
+              message: "판매 가격은 최대 100자 이내여야 합니다.",
+            },
+            pattern: {
+              value: /^\d+$/,
+              message: "숫자만 입력해주세요.",
+            },
+          })}
           onBlur={handleBlur}
         />
         {(formErrors.price || validationErrors.price) && (
@@ -156,14 +218,13 @@ function Registration() {
           type="text"
           placeholder="태그를 입력해주세요"
           onKeyDown={handleTagKeyPress}
-          {...register("tags")}
-          onBlur={handleBlur}
+          value={newTag}
+          onChange={(e) => setNewTag(e.target.value)}
         />
-        <TagList
-          tags={tags}
-          formErrors={formErrors}
-          validationErrors={validationErrors}
-        />
+        {formErrors.tags && (
+          <span className={styles.error}>{formErrors.tags.message}</span>
+        )}
+        <TagList tags={allTags} handleTagDelete={handleTagDelete} />
       </fieldset>
     </form>
   );
--- src/components/TagList/TagList.jsx
+++ src/components/TagList/TagList.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import styles from "./TagList.module.css";
 import closeIcon from "../../assets/svg/X-icon.svg";
 
-const TagList = ({ tags, formErrors, validationErrors }) => (
-  <>
-    {(formErrors.tags || validationErrors.tags) && (
-      <span className={styles.error}>
-        {formErrors.tags ? formErrors.tags.message : validationErrors.tags}
-      </span>
-    )}
-    <div className={styles.tag_list}>
-      {tags?.map((tag, index) => (
-        <div key={index} className={styles.tag_chip}>
-          {tag}
-          <img src={closeIcon} alt="닫기 아이콘" width="20" height="20" />
-        </div>
-      ))}
-    </div>
-  </>
+const TagList = ({ tags, handleTagDelete }) => (
+  <div className={styles.tag_list}>
+    {tags.map((tag, index) => (
+      <div key={index} className={styles.tag_chip}>
+        {tag}
+        <img
+          src={closeIcon}
+          alt="닫기 아이콘"
+          width="20"
+          height="20"
+          onClick={() => handleTagDelete(tag)}
+        />
+      </div>
+    ))}
+  </div>
 );
 
 export default TagList;
